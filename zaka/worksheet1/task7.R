f6a24a48755d3efe03a142dbe7103a6ed99d6712
zaka <103366844+zakaBouj@users.noreply.github.com>
2024-10-24 13:13:00 +0200

    Create task7.R

---
+++ zaka/worksheet1/task7.R
@@ -0,0 +1,8 @@
+# Task 7
+# The R dataset mpg
+#   (a) Inspect the dataset mpg.
+library(tidyverse)
+head(mpg)
+data(mpg)
+
+#   (b) Determine the types and the scales of measurement of all variables in the dataset mpg. Further more determine whether the variables are discret or continous.
